0f312bb90c9d859445a7efbfe6525dbb1c063c74
ehannum <ehannum@gmail.com>
2013-12-13 21:16:05 -0800

    more heroku tinkering

--- personal-server.js
+++ personal-server.js
@@ -63,5 +63,5 @@ var port = 8080;
 // var ip = '127.0.0.1';
 var server = http.createServer(requestHandler);
 
-console.log('Listening on http://' + ip + ':' + port);
+//console.log('Listening on http://' + ip + ':' + port);
 server.listen(port);
